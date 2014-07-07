6901c7c347f5edec4e4e18382a73e63b436d9d0f
Laura Filman <lmfilman@gmail.com>
2014-07-07 18:58:38 -0400

    adding initial files copied from p1 repo

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>P1 - Laura Filman - Dynamic Web Applications</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
+			body {
+				font:15px arial,sans-serif;
+				margin: 20px;
+			}
+            .centerAndColor {
+            	margin: auto;
+            	background-color: #FF9966;
+            	text-align:center;
+            	color: #FFFFFF;
+            }
+            .center {
+                margin: auto;
+                text-align:center;
+            }
+            p {
+                width: 700px;
+                padding: 10px;
+                border-width: 5px;
+                border-color: #FF9966;
+                border-style: solid;
+                margin: 0px;
+            }
+		</style>
+	</head>
+	<body>
+		<div>
+			<div class="centerAndColor"><h1>P1 - Laura Filman - Dynamic Web Applications</h1></div>
+			<div class="center"><img src="images/harvardid.jpg" alt="Laura" title="Laura" ></div>
+			<h2>Questions:</h2>
+			<h3>In 2+ sentences describe who you are and what you do.</h3>
+				<p>Hi! My name is Laura, and I'm a QE (quality engineer) at MathWorks (they make MATLAB). I primarily work on verifying and writing automated tests for the web applications on <a href="http://www.mathworks.com/matlabcentral/">MATLAB Central</a>.  In my spare time, I like to jog, cook, and watch Netflix.</p>
+			<h3>Why are you taking this course?</h3>
+				<p>I'm taking Dynamic Web Applications because I'd like to gain more experience with web applications from a developer's perspective, rather than as a QE.  I'd like to gain a better understanding of the whole system and how all the pieces fit together.</p>
+			<h3>What previous experience do you have with HTML/CSS?</h3>
+				<p>I have limited experience with HTML/CSS.  I have never written it myself before now.</p>
+			<h3>What previous experience do you have with programming?</h3>
+				<p>I have solid experience with programming. I received my B.A. in Computer Science in 2013, and I use my programming knowledge on a day-to-day basis writing automated tests in Java.</p>
+			<h3>Have you worked with PHP before? If so, to what extent?</h3>
+				<p>I worked with PHP <em>once</em>, using my knowledge of other programming languages to help my mom debug a couple issues on her PHP-based web site.</p>
+			<h3>Are you working on a Mac, PC, or some other platform?</h3>
+				<p>I am working on a PC.</p>
+			<h3>Anything else we should know?</h3>
+				<p>Not that I can think of :)</p>
+		</div>
+	</body>
+
+	
+</html>
